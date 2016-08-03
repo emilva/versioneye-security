6b049a2b39632aac3b15bd5b1069921cd467739c
reiz <robert.reiz.81@gmail.com>
2016-08-03 16:47:21 +0200

    Fix tests

--- spec/versioneye/node_security_crawler_spec.rb
+++ spec/versioneye/node_security_crawler_spec.rb
@@ -10,6 +10,8 @@ describe NodeSecurityCrawler do
       product.versions.push( Version.new( { :version => "1.1.0" } ) )
       product.versions.push( Version.new( { :version => "2.0.0" } ) )
       product.versions.push( Version.new( { :version => "2.1.0" } ) )
+      product.versions.push( Version.new( { :version => "3.0.0" } ) )
+      product.versions.push( Version.new( { :version => "3.1.0" } ) )
       expect( product.save ).to be_truthy
 
       worker = Thread.new{ SecurityWorker.new.work }
@@ -21,8 +23,10 @@ describe NodeSecurityCrawler do
 
       product = Product.fetch_product Product::A_LANGUAGE_NODEJS, 'jws'
       expect( product.version_by_number('1.1.0').sv_ids ).to_not be_empty
-      expect( product.version_by_number('2.0.0').sv_ids ).to be_empty
-      expect( product.version_by_number('2.1.0').sv_ids ).to be_empty
+      expect( product.version_by_number('2.0.0').sv_ids ).to_not be_empty
+      expect( product.version_by_number('2.1.0').sv_ids ).to_not be_empty
+      expect( product.version_by_number('3.0.0').sv_ids ).to be_empty
+      expect( product.version_by_number('3.1.0').sv_ids ).to be_empty
     end
 
   end
