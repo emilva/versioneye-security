5e5f04a2ac527cae6d3225672dfb3523396b6a35
reiz <robert.reiz.81@gmail.com>
2016-11-13 22:43:43 +0100

    Add first version of NVD crawler

--- lib/versioneye-security.rb
+++ lib/versioneye-security.rb
@@ -10,6 +10,7 @@ require './lib/versioneye/java_security_crawler'
 require './lib/versioneye/python_security_crawler'
 require './lib/versioneye/node_security_crawler'
 require './lib/versioneye/snyk_security_crawler'
+require './lib/versioneye/nvd_security_crawler'
 
 require './lib/versioneye/producers/producer'
 require './lib/versioneye/producers/security_producer'
---
+++ lib/versioneye/constants/nvd_mapping.rb
@@ -0,0 +1,122 @@
+class NvdMapping
+
+  A_MAPPING = {
+    "apache:apache_commons_daemon"=> {
+      "Maven" => [
+        "commons-daemon:commons-daemon"
+      ]
+    },
+    "apache:commons-compress"=> {
+      "Maven" => [
+        "org.apache.commons:commons-compress"
+      ]
+    },
+    "apache:commons-httpclient"=> {
+      "Maven" => [
+        "org.apache.httpcomponents:httpclient"
+      ]
+    },
+    "apache:commons_fileupload"=> {
+      "Maven" => [
+        "commons-fileupload:commons-fileupload",
+        "apache-fileupload:commons-fileupload"
+      ]
+    },
+    "apache:struts" => {
+      "Maven" => [
+        "org.apache.struts:struts2-core",
+        "struts:struts",
+      ]
+    },
+    "sun:jsf" => {
+      "Maven" => [
+        "com.sun:jsf-impl",
+        "com.sun.faces:jsf-impl",
+        "com.sun.faces:jsf-api"
+      ]
+    },
+    "vaadin:vaadin" => {
+      "Maven" => ["com.vaadin:vaadin"]
+    },
+    "pivotal_software:spring_data_jpa" => {
+      "Maven" => ["org.springframework.data:spring-data-jpa"]
+    },
+    "pivotal_software:spring_framework" => {
+      "Maven" => [
+        "org.springframework:spring-core",
+        "org.springframework:spring-agent",
+        "org.springframework:spring-aop",
+        "org.springframework:spring-asm",
+        "org.springframework:spring-aspects",
+        "org.springframework:spring-beandoc",
+        "org.springframework:spring-beans",
+        "org.springframework:spring-binding",
+        "org.springframework:spring-context",
+        "org.springframework:spring-context-support",
+        "org.springframework:spring-dao",
+        "org.springframework:spring-datastore-core",
+        "org.springframework:spring-datastore-gae",
+        "org.springframework:spring-datastore-gemfire",
+        "org.springframework:spring-datastore-jpa",
+        "org.springframework:spring-datastore-mock",
+        "org.springframework:spring-datastore-mongo",
+        "org.springframework:spring-datastore-redis",
+        "org.springframework:spring-datastore-riak",
+        "org.springframework:spring-datastore-simple",
+        "org.springframework:spring-datastore-web",
+        "org.springframework:spring-expression",
+        "org.springframework:spring-faces",
+        "org.springframework:spring-framework-bom",
+        "org.springframework:spring-full",
+        "org.springframework:spring-hibernate",
+        "org.springframework:spring-hibernate2",
+        "org.springframework:spring-hibernate3",
+        "org.springframework:spring-ibatis",
+        "org.springframework:spring-instrument",
+        "org.springframework:spring-instrument-classloading",
+        "org.springframework:spring-instrument-tomcat",
+        "org.springframework:spring-integration-tests",
+        "org.springframework:spring-javaconfig",
+        "org.springframework:spring-jca",
+        "org.springframework:spring-jdbc",
+        "org.springframework:spring-jdo",
+        "org.springframework:spring-jms",
+        "org.springframework:spring-jmx",
+        "org.springframework:spring-jpa",
+        "org.springframework:spring-ldap",
+        "org.springframework:spring-library",
+        "org.springframework:spring-messaging",
+        "org.springframework:spring-mock",
+        "org.springframework:spring-ojb",
+        "org.springframework:spring-orm",
+        "org.springframework:spring-oxm",
+        "org.springframework:spring-parent",
+        "org.springframework:spring-portlet",
+        "org.springframework:spring-remoting",
+        "org.springframework:spring-retry",
+        "org.springframework:spring-struts",
+        "org.springframework:spring-support",
+        "org.springframework:spring-test",
+        "org.springframework:spring-test-htmlunit",
+        "org.springframework:spring-test-htmlunit-build",
+        "org.springframework:spring-test-mvc",
+        "org.springframework:spring-test-mvc-htmlunit",
+        "org.springframework:spring-tomcat-weaver",
+        "org.springframework:spring-toplink",
+        "org.springframework:spring-tuple",
+        "org.springframework:spring-tuple-parent",
+        "org.springframework:spring-tx",
+        "org.springframework:spring-web",
+        "org.springframework:spring-webflow",
+        "org.springframework:spring-webmvc",
+        "org.springframework:spring-webmvc-portlet",
+        "org.springframework:spring-webmvc-struts",
+        "org.springframework:spring-websocket",
+        "org.springframework:springloaded",
+        "org.springframework.data:spring-data-jpa"
+      ]
+    }
+  }
+
+
+end
